4e5d9b2b695dc167e2faba566c8edf42a77d04b4
ladyleane <q-by@msn.com>
2018-11-07 23:14:00 +0800

    Modify MNIST about tf.logging

--- MNIST.py
+++ MNIST.py
@@ -1,6 +1,9 @@
 import numpy as np
 import tensorflow as tf
 
+old_v = tf.logging.get_verbosity()
+tf.logging.set_verbosity(tf.logging.ERROR)
+
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot = True)
 
